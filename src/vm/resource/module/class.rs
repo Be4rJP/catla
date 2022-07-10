01e6554b1628818941a7187209d4d791605281aa
Be4rJP <bea0224@outlook.jp>
2022-07-11 01:08:04 +0900

    ヨシ！

--- src/vm/resource/module/class.rs
+++ src/vm/resource/module/class.rs
@@ -1,11 +1,12 @@
+use std::borrow::BorrowMut;
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 use once_cell::sync::Lazy;
 
-static CLASS_PROVIDER: Lazy<ClassProvider> = Lazy::new(|| {ClassProvider});
+static mut CLASS_PROVIDER: Lazy<ClassProvider> = Lazy::new(|| {ClassProvider});
 
-pub struct ClassProvider {
-    pub class_pool: RwLock<HashMap<&'static str, Arc<Class>>>
+struct ClassProvider {
+    pub class_pool: RwLock<HashMap<&'static str, Arc<ClassAccessor>>>
 }
 
 impl ClassProvider {
@@ -14,9 +15,30 @@ impl ClassProvider {
             class_pool: RwLock::new(HashMap::new())
         }
     }
+
+    pub fn register(&mut self, class: Arc<ClassAccessor>) {
+        self.class_pool.write().unwrap().insert(class.name, class);
+    }
+}
+
+fn register_class(class: Arc<ClassAccessor>) {
+    unsafe {
+        CLASS_PROVIDER.register(class);
+    }
 }
 
 
-pub struct Class {
+pub struct ClassAccessor {
+    pub name: &'static str
+}
 
+impl ClassAccessor {
+    pub fn new(name: &'static str) -> Arc<Self> {
+        let st = Self {
+            name
+        };
+        let instance = Arc::new(st);
+        register_class(instance.clone());
+        return instance;
+    }
 }
